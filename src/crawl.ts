65bfd3145d14127938993f540d1d54a945f5fe62
raisama21 <freekinguse21@gmail.com>
2023-10-16 13:09:55 +0545

    added new function to crawl page and handle error

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,5 +1,33 @@
 import { JSDOM } from "jsdom";
 
+export async function crawlPage(currentURL: string) {
+    console.log(`actively crawling ${currentURL}`);
+
+    try {
+        const response = await fetch(currentURL);
+        if (response.status > 399) {
+            console.log(
+                `error in fetch with status code: ${response.status} on page: ${currentURL}`
+            );
+            return;
+        }
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType?.includes("text/html")) {
+            console.log(
+                `non html response, content type: ${contentType} on page: ${currentURL}`
+            );
+            return;
+        }
+
+        const data = await response.text();
+
+        console.log(data);
+    } catch (error: any) {
+        console.log(`error in fetch, ${error.message}, on page ${currentURL}`);
+    }
+}
+
 export function getURLsFromHTML(htmlBody: string, baseURL: string): string[] {
     const urls: string[] = [];
     const dom = new JSDOM(htmlBody);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("hello world!");
+import { crawlPage } from "./crawl.ts";
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided");
+
+        process.exit(1);
+    }
+
+    if (process.argv.length > 3) {
+        console.log("Too many command line args");
+
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+
+    console.log(`starting crawl of ${baseURL}`);
+
+    crawlPage(baseURL);
+}
+
+main();
